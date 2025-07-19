0a7aef024443aa022caece4ae8e43a585de7a023
muhammad1249ahmad <>
2025-07-19 20:49:24 +0500

    Created avSlice,venueSlice,mealSlice and used them in main component accordingly-Completed

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,51 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      name:"Projectors",
+      cost:200,
+      quantity:0,
+      img:"https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289_1280.jpg"
+    },
+    {
+      name:"Speaker",
+      cost:35,
+      quantity:0,
+      img:"https://cdn.pixabay.com/photo/2018/08/27/10/11/radio-cassette-3634616_1280.png"
+    },
+    {
+      name:"Microphones",
+      cost:45,
+      quantity:0,
+      img:"https://cdn.pixabay.com/photo/2024/11/30/07/22/ai-generated-9234256_1280.jpg"
+    },
+    {
+      name:"Whiteboards",
+      cost:80,
+      quantity:0,
+      img:"https://cdn.pixabay.com/photo/2024/07/28/15/11/ai-generated-8927764_1280.jpg"
+    },
+    {
+      name:"Signage",
+      cost:80,
+      quantity:0,
+      img:"https://cdn.pixabay.com/photo/2014/12/21/23/53/sign-576261_1280.png"
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const toInc=state[action.payload];
+      if(toInc){
+        toInc.quantity+=1;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      const toDec=state[action.payload];
+      if(toDec && toDec.quantity>0){
+        toDec.quantity-=1;
+      }
     },
   },
 });
--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,56 @@
 // mealsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-export const mealsSlice = createSlice({
-  name: 'meals',
+const mealsSlice = createSlice({
+  name: "meal",
   initialState: [
-   
+    {
+      name: "Breakfast",
+      cost: 50,
+      selected: false,
+      type: "mealForPeople",
+      numberOfPeople: 0,
+    },
+    {
+      name: "High Tea",
+      cost: 25,
+      selected: false,
+      type: "flat",
+      numberOfPeople: 0,
+    },
+    {
+      name: "Lunch",
+      cost: 65,
+      selected: false,
+      type: "mealForPeople",
+      numberOfPeople: 0,
+    },
+    {
+      name: "Dinner",
+      cost: 70,
+      selected: false,
+      type: "mealForPeople",
+      numberOfPeople: 0,
+    },
   ],
   reducers: {
-    toggleMealSelection: (state, action) => {
+    toggleMealSelection(state, action) {
+      const { index } = action.payload;
+      state[index].selected = !state[index].selected;
+    },
+    updateNumberOfPeople(state, action) {
+      const { numberOfPeople } = action.payload;
+      state.forEach((meal) => {
+        if (meal.type === "mealForPeople") {
+          meal.numberOfPeople = numberOfPeople;
+        }
+        else{
+          meal.numberOfPeople=numberOfPeople==0?numberOfPeople:1;
+        }
+      });
     },
   },
 });
 
-export const { toggleMealSelection } = mealsSlice.actions;
-
+export const { toggleMealSelection, updateNumberOfPeople } = mealsSlice.actions;
 export default mealsSlice.reducer;
